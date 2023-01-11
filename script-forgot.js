95ebcea9c5373cec59e51438ea3456dfc108d7b2
Douglas <douglasarthur.s.bento@gmail.com>
2023-01-11 11:35:47 -0400

    weblogin

---
+++ script-forgot.js
@@ -0,0 +1,13 @@
+const chk = document.getElementById('chk')
+
+
+
+chk.addEventListener('change', () =>{
+    document.body.classList.toggle('dark')
+    document.querySelector(".mainlogin").classList.toggle('dark')
+    document.querySelector(".login").classList.toggle('dark')
+    document.querySelector(".title").classList.toggle('dark')
+    document.querySelector(".text-confirm").classList.toggle('dark')
+    document.querySelector(".lab-email").classList.toggle('dark')
+    
+})
---
+++ script.js
@@ -0,0 +1,17 @@
+const chk = document.getElementById('chk')
+
+
+
+chk.addEventListener('change', () =>{
+    document.body.classList.toggle('dark')
+    document.querySelector(".mainlogin").classList.toggle('dark')
+    document.querySelector(".siginup").classList.toggle('dark')
+    document.querySelector(".login").classList.toggle('dark')
+    document.querySelector(".lab-user").classList.toggle('dark')
+    document.querySelector(".lab-pass").classList.toggle('dark')
+    document.querySelector(".lab-confirm").classList.toggle('dark')
+    document.querySelector(".lab-email").classList.toggle('dark')
+
+    document.querySelector(".title").classList.toggle('dark')
+    
+})
