00ef7d665132cf09824d3fea03265121090b9e03
Douglas <douglasarthur.s.bento@gmail.com>
2023-01-12 15:56:57 -0400

    weblogin

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ chk.addEventListener('change', () =>{
     document.querySelector(".mainlogin").classList.toggle('dark')
     document.querySelector(".siginup").classList.toggle('dark')
     document.querySelector(".login").classList.toggle('dark')
+    document.querySelector(".remember").classList.toggle('dark')
     document.querySelector(".lab-user").classList.toggle('dark')
     document.querySelector(".lab-pass").classList.toggle('dark')
     document.querySelector(".lab-confirm").classList.toggle('dark')
